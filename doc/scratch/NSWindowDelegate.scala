152017144ec54eabb23b45b7f66bdf7ae9c65202
Jeremy Cloud <jeremy.cloud@me.com>
2009-06-19 15:19:04 -0400

    cleanup

--- doc/scratch/NSWindowDelegate.scala
+++
@@ -1,6 +0,0 @@
-class MyWindowDelegate extends NSObject {
-    @message("(BOOL)window:(NSWindow *)sender shouldDragDocumentWithEvent:(NSEvent *)mouseEvent from:(NSPoint)startPoint withPasteboard:(NSPasteboard *)pasteboard")
-    def windowShouldDragDocumentWithEventFromWithPasteboard(window: NSWindow, mouseEvent: NSEvent, startPoint: NSPoint, pasteboard: NSPasteboard): Boolean = {
-        window!(`window->window, `shouldDragDocumentWithEvent->mouseEvent, `from->startPoint, `withPasteboard->pasteboard)
-    }
-}
